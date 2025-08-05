ddab65e5bcd2cb43095dc1a633299bd282008f0c
Sahil Raj <sahilraj7094@gmail.com>
2025-08-05 11:58:57 +0530

    Is Subsequence.cpp

--- Is Subsequence.cpp
+++ Is Subsequence.cpp
@@ -28,3 +28,22 @@ public:
         return i==s.length();        
     }
 };
+
+// Recursive :-
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        if(s.size()==0){
+            return true;
+        }
+        if(t.size()==0){
+            return false;
+        }
+        if(s[0]==t[0]){
+            return isSubsequence(s.substr(1),t.substr(1));
+        }
+        else{
+            return isSubsequence(s,t.substr(1));
+        }
+    }
+};
