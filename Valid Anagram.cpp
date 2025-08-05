82d12c27092d60327424b45f6093cdcffb6a7413
Sahil Raj <sahilraj7094@gmail.com>
2025-08-06 03:17:02 +0530

    Valid Anagram.cpp

--- Valid Anagram.cpp
+++ Valid Anagram.cpp
@@ -19,3 +19,20 @@ public:
         return s==t;
     }
 };
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.size() != t.size()) return false;
+        int count[26] = {0};
+
+        for (int i=0; i<s.size(); i++) {
+            count[s[i]-'a']++;
+            count[t[i]-'a']--;
+        }
+        for (int i=0; i<26; i++) {
+            if (count[i]!=0) return false;
+        }
+        return true;
+    }
+};
